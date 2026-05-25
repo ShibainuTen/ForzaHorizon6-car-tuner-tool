b037d2b7c4f80200e74cf5055873c0fc3c1fcf91
ShibainuTen <wnb0530@gmail.com>
2026-05-25 22:19:35 +0900

    fix: CRAとTypeScript環境への完全な原点回帰

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,14 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
 
-// HTMLから 'root' 要素を取得
 const rootElement = document.getElementById('root');
 
-// もし rootElement が存在したら、それをターゲットにしてReactアプリを起動
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
